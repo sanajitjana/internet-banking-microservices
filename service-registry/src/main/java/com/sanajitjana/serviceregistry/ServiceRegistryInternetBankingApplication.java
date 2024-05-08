d585b784229fedcc039dfb71dfb4c32c61237985
Sanajit Jana <sanajit.jana@mplifytech.com>
2024-05-08 19:48:32 +0530

    Add Eureka Server as Service Registry

---
+++ service-registry/src/main/java/com/sanajitjana/serviceregistry/ServiceRegistryInternetBankingApplication.java
@@ -0,0 +1,13 @@
+package com.sanajitjana.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryInternetBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryInternetBankingApplication.class, args);
+	}
+
+}
---
+++ service-registry/src/test/java/com/sanajitjana/serviceregistry/ServiceRegistryInternetBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanajitjana.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryInternetBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
